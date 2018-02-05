592eae867678d78b1cfd844f712ffad1c5ff05f2
mino <mino@mino.local>
2018-02-05 20:18:47 +0900

    [추가] tslint 설정파일 추가

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res, next) => {
   res.status(200).json({ message: 'hello' });
-})
+});
 
 app.listen(8080, () => {
   console.log('express server start');
