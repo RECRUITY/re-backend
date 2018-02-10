029d662fa66aa8f2a6a492bb04346922afb4c513
mino <mino@mino.local>
2018-02-10 16:05:22 +0900

    [추가] mongoose 연결 추가

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 /* External dependencies */
 import * as express from 'express';
+import * as mongoose from 'mongoose';
+
+/* Internal dependencies */
+import secret from './secret';
 
 const app = express();
+
+/* Connect to mongodb */
+mongoose.connect(secret.MONGO_DB, { promiseLibrary: global.Promise }, (err): void => {
+  if (err) {
+    console.log('Occurred the error when connecting mongodb: ', err);
+  }
+});
 
 app.get('/', (req, res, next) => {
   res.status(200).json({ message: 'hello' });
