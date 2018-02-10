660419ab91b4047618fd49b73553bbcdfaa4f040
mino <mino@mino.local>
2018-02-10 17:50:42 +0900

    [추가] 기본적인 미들웨어 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 /* External dependencies */
 import * as express from 'express';
 import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as compression from 'compression';
 
 /* Internal dependencies */
 import secret from './secret';
@@ -13,6 +16,15 @@ mongoose.connect(secret.MONGO_DB, { promiseLibrary: global.Promise }, (err): voi
     console.log('Occurred the error when connecting mongodb: ', err);
   }
 });
+
+/* Apply Middleware */
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
 app.get('/', (req, res, next) => {
   res.status(200).json({ message: 'hello' });
